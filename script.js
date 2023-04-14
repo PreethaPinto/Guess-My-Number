5ceaef79ec0dcd025c112c9002bffbbeced46280
PreethaPinto <preethacp@gmail.com>
2023-04-14 14:20:02 +1000

    Added event listeners

---
+++ script.js
@@ -0,0 +1,50 @@
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = (message) => {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  //No number
+  if (!guess) {
+    displayMessage("⛔ No Number");
+
+    //Correct Number
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number");
+    document.querySelector(".randomNumber").textContent = secretNumber;
+    //document.querySelector("body").style.backgroundColor = "#60b347";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    //Wrong guess
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "📈 Too high" : "📉 Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".randomNumber").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
